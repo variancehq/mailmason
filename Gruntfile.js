e7199642b88a0e2a658d5c2b4c7a5aa4c16f7c7c
Guillaume Mathieu <contact@gmathieu.com>
2021-08-18 13:03:24 -0700

    style: prettier

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,34 @@
-module.exports = function(grunt) {
-  const sass = require('node-sass')
-  require('load-grunt-tasks')(grunt, { pattern: ['grunt-*', 'assemble'] })
-  require('time-grunt')(grunt)
-  grunt.loadNpmTasks('grunt-ftp-deploy')
+module.exports = function (grunt) {
+  const sass = require("node-sass");
+  require("load-grunt-tasks")(grunt, { pattern: ["grunt-*", "assemble"] });
+  require("time-grunt")(grunt);
+  grunt.loadNpmTasks("grunt-ftp-deploy");
 
   const path = {
-    css_src: 'src/stylesheets/',
-    dist: 'dist/',
-    dist_html_glob: 'dist/**/*.html',
-    dist_text_glob: 'dist/**/*.txt',
-    templates: 'src/templates/',
-    layouts: 'src/layouts/',
-    partials: 'src/partials/',
-    images_src: 'src/images',
-  }
+    css_src: "src/stylesheets/",
+    dist: "dist/",
+    dist_html_glob: "dist/**/*.html",
+    dist_text_glob: "dist/**/*.txt",
+    templates: "src/templates/",
+    layouts: "src/layouts/",
+    partials: "src/partials/",
+    images_src: "src/images",
+  };
 
   /* Configuration
   ================================================= */
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     /* secrets.json and config.json simplify variable management to keep the
        common changes centralized to key files instead of being littered
        throughout the Gruntfile. This also makes it easy to .gitignore secrets
     ================================================= */
-    secret: grunt.file.exists('secrets.json')
-      ? grunt.file.readJSON('secrets.json')
+    secret: grunt.file.exists("secrets.json")
+      ? grunt.file.readJSON("secrets.json")
       : {},
-    config: grunt.file.readJSON('config.json'),
+    config: grunt.file.readJSON("config.json"),
 
     /* SASS
     ------------------------------------------------- */
@@ -40,8 +40,8 @@ module.exports = function(grunt) {
       styles: {
         expand: true,
         cwd: path.css_src,
-        ext: '.css',
-        src: ['*.scss'],
+        ext: ".css",
+        src: ["*.scss"],
         dest: `${path.dist}stylesheets/`,
       },
     },
@@ -51,13 +51,13 @@ module.exports = function(grunt) {
 
     autoprefixer: {
       options: {
-        browsers: ['last 6 versions', 'ie >= 9'],
+        browsers: ["last 6 versions", "ie >= 9"],
       },
       styles: {
         expand: true,
         cwd: `${path.dist}styles/`,
-        ext: '.css',
-        src: ['*.css'],
+        ext: ".css",
+        src: ["*.css"],
         dest: `${path.dist}styles/`,
       },
     },
@@ -72,42 +72,42 @@ module.exports = function(grunt) {
         layoutdir: path.layouts,
         partials: `${path.partials}*.hbs`,
         flatten: true,
-        sender_name: '<%= config.strings.sender_name %>',
-        product_name: '<%= config.strings.product_name %>',
-        product_url: '<%= config.strings.product_url %>',
+        sender_name: "<%= config.strings.sender_name %>",
+        product_name: "<%= config.strings.product_name %>",
+        product_url: "<%= config.strings.product_url %>",
         credit_card_statement_name:
-          '<%= config.strings.credit_card_statement_name %>',
-        formal_company_name: '<%= config.strings.formal_company_name %>',
-        address_line_1: '<%= config.strings.address_line_1 %>',
-        address_line_2: '<%= config.strings.address_line_2 %>',
-        city: '<%= config.strings.city %>',
-        state: '<%= config.strings.state %>',
-        country: '<%= config.strings.country %>',
-        phone: '<%= config.strings.phone %>',
-        images_url: '<%= config.images.images_url %>',
-        use_images: '<%= config.images.use_images %>',
-        logo_file: '<%= config.images.logo_file %>',
-        logo_width: '<%= config.images.logo_width %>',
-        use_social_circles: '<%= config.images.use_social_circles %>',
-        twitter_url: '<%= config.images.twitter_url %>',
-        facebook_url: '<%= config.images.facebook_url %>',
-        pinterest_url: '<%= config.images.pinterest_url %>',
-        instagram_url: '<%= config.images.instagram_url %>',
-        dribbble_url: '<%= config.images.dribbble_url %>',
-        google_plus_url: '<%= config.images.google_plus_url %>',
-        youtube_url: '<%= config.images.youtube_url %>',
-        linkedin_url: '<%= config.images.linkedin_url %>',
+          "<%= config.strings.credit_card_statement_name %>",
+        formal_company_name: "<%= config.strings.formal_company_name %>",
+        address_line_1: "<%= config.strings.address_line_1 %>",
+        address_line_2: "<%= config.strings.address_line_2 %>",
+        city: "<%= config.strings.city %>",
+        state: "<%= config.strings.state %>",
+        country: "<%= config.strings.country %>",
+        phone: "<%= config.strings.phone %>",
+        images_url: "<%= config.images.images_url %>",
+        use_images: "<%= config.images.use_images %>",
+        logo_file: "<%= config.images.logo_file %>",
+        logo_width: "<%= config.images.logo_width %>",
+        use_social_circles: "<%= config.images.use_social_circles %>",
+        twitter_url: "<%= config.images.twitter_url %>",
+        facebook_url: "<%= config.images.facebook_url %>",
+        pinterest_url: "<%= config.images.pinterest_url %>",
+        instagram_url: "<%= config.images.instagram_url %>",
+        dribbble_url: "<%= config.images.dribbble_url %>",
+        google_plus_url: "<%= config.images.google_plus_url %>",
+        youtube_url: "<%= config.images.youtube_url %>",
+        linkedin_url: "<%= config.images.linkedin_url %>",
       },
       generic: {
         expand: true,
         cwd: path.templates,
-        src: ['**/*.hbs'],
+        src: ["**/*.hbs"],
         dest: `${path.dist}compiled`,
       },
       postmark: {
         expand: true,
         cwd: path.dist,
-        src: ['postmark-templates/**/*.hbs', 'postmark-layouts/**/*.hbs'],
+        src: ["postmark-templates/**/*.hbs", "postmark-layouts/**/*.hbs"],
         dest: path.dist,
       },
     },
@@ -121,15 +121,15 @@ module.exports = function(grunt) {
       postmark: {
         expand: true,
         cwd: path.dist,
-        ext: '.html',
-        src: ['postmark-layouts/**/*.html'],
+        ext: ".html",
+        src: ["postmark-layouts/**/*.html"],
         dest: path.dist,
       },
       generic: {
         expand: true,
         cwd: path.dist,
-        ext: '.html',
-        src: ['compiled/**/*.html'],
+        ext: ".html",
+        src: ["compiled/**/*.html"],
         dest: path.dist,
       },
     },
@@ -143,15 +143,15 @@ module.exports = function(grunt) {
       postmark: {
         expand: true,
         cwd: path.dist,
-        ext: '.html',
-        src: ['postmark-layouts/**/*.html'],
+        ext: ".html",
+        src: ["postmark-layouts/**/*.html"],
         dest: path.dist,
       },
       generic: {
         expand: true,
         cwd: path.dist,
-        ext: '.html',
-        src: ['compiled/**/*.html'],
+        ext: ".html",
+        src: ["compiled/**/*.html"],
         dest: path.dist,
       },
     },
@@ -163,7 +163,7 @@ module.exports = function(grunt) {
 
     premailer: {
       options: {
-        warnLevel: 'none',
+        warnLevel: "none",
         preserveStyles: true,
         preserveStyleAttribute: true,
       },
@@ -172,21 +172,21 @@ module.exports = function(grunt) {
           {
             expand: true,
             src: [`${path.dist}compiled/**/*.html`],
-            dest: '',
+            dest: "",
           },
         ],
       },
       txt: {
         options: {
-          mode: 'txt',
+          mode: "txt",
           lineLength: 16384,
         },
         files: [
           {
             expand: true,
             src: [path.dist_html_glob],
-            dest: '',
-            ext: '.txt',
+            dest: "",
+            ext: ".txt",
           },
         ],
       },
@@ -196,19 +196,19 @@ module.exports = function(grunt) {
     Deploy assets to FTP
     ------------------------------------------------- */
 
-    'ftp-deploy': {
+    "ftp-deploy": {
       build: {
         auth: {
-          host: '<%= config.ftp.host %>',
-          port: '<%= config.ftp.port %>',
-          username: '<%= secret.ftp.username %>',
-          password: '<%= secret.ftp.password %>',
+          host: "<%= config.ftp.host %>",
+          port: "<%= config.ftp.port %>",
+          username: "<%= secret.ftp.username %>",
+          password: "<%= secret.ftp.password %>",
         },
-        src: '<%= config.ftp.src %>',
-        dest: '<%= config.ftp.dest %>',
+        src: "<%= config.ftp.src %>",
+        dest: "<%= config.ftp.dest %>",
         exclusions: [
-          'path/to/source/folder/**/.DS_Store',
-          'path/to/source/folder/**/Thumbs.db',
+          "path/to/source/folder/**/.DS_Store",
+          "path/to/source/folder/**/Thumbs.db",
         ],
       },
     },
@@ -220,15 +220,15 @@ module.exports = function(grunt) {
 
     s3: {
       options: {
-        accessKeyId: '<%= secret.s3.access_key_id %>',
-        secretAccessKey: '<%= secret.s3.secret_access_key %>',
-        bucket: '<%= config.s3.bucket %>',
-        region: '<%= config.s3.region %>',
-        overwrite: '<%= config.s3.overwrite %>',
+        accessKeyId: "<%= secret.s3.access_key_id %>",
+        secretAccessKey: "<%= secret.s3.secret_access_key %>",
+        bucket: "<%= config.s3.bucket %>",
+        region: "<%= config.s3.region %>",
+        overwrite: "<%= config.s3.overwrite %>",
       },
       build: {
         cwd: path.images_src,
-        src: '**',
+        src: "**",
       },
     },
 
@@ -240,8 +240,8 @@ module.exports = function(grunt) {
       options: {
         livereload: 3000,
       },
-      files: ['src/**/*', './preview.html'],
-      tasks: ['default'],
+      files: ["src/**/*", "./preview.html"],
+      tasks: ["default"],
     },
 
     /* Replace
@@ -253,7 +253,7 @@ module.exports = function(grunt) {
         src: [`${path.dist}postmark-layouts/**/content.hbs`],
         overwrite: true,
         replacements: [
-          { from: /({{)(.?body.?)(}})/g, to: '\\{{{ @content }}}' },
+          { from: /({{)(.?body.?)(}})/g, to: "\\{{{ @content }}}" },
         ],
       },
 
@@ -262,7 +262,10 @@ module.exports = function(grunt) {
         src: [`${path.dist}postmark-layouts/**/content.hbs`],
         overwrite: true,
         replacements: [
-          { from: /({{)(.?pm:preheader.?)(}})/g, to: '\\{{#preheader}}<span class="preheader">\\{{.}}</span>\\{{/preheader}}' },
+          {
+            from: /({{)(.?pm:preheader.?)(}})/g,
+            to: '\\{{#preheader}}<span class="preheader">\\{{.}}</span>\\{{/preheader}}',
+          },
         ],
       },
 
@@ -270,7 +273,7 @@ module.exports = function(grunt) {
       postmarkConfig: {
         src: [`${path.dist}postmark-templates/**/content.hbs`],
         overwrite: true,
-        replacements: [{ from: /(---)(.*)(---)/s, to: '' }],
+        replacements: [{ from: /(---)(.*)(---)/s, to: "" }],
       },
 
       // Premailer escapes URLs, so our mustachio variables for URLs get fully escaped, and the
@@ -279,13 +282,13 @@ module.exports = function(grunt) {
         src: [path.dist_text_glob],
         overwrite: true,
         replacements: [
-          { from: '%7B%7B%7B', to: '{{{ ' },
-          { from: '%7D%7D%7D', to: ' }}}' },
-          { from: '%7D%7D%7D%22', to: ' }}}' },
-          { from: '%7B%7B', to: '{{ ' },
-          { from: '%7D%7D', to: ' }}' },
-          { from: '%7D%7D%22', to: ' }}' },
-          { from: '%20', to: '' },
+          { from: "%7B%7B%7B", to: "{{{ " },
+          { from: "%7D%7D%7D", to: " }}}" },
+          { from: "%7D%7D%7D%22", to: " }}}" },
+          { from: "%7B%7B", to: "{{ " },
+          { from: "%7D%7D", to: " }}" },
+          { from: "%7D%7D%22", to: " }}" },
+          { from: "%20", to: "" },
         ],
       },
 
@@ -295,7 +298,7 @@ module.exports = function(grunt) {
         overwrite: true,
         replacements: [
           {
-            from: '<style>',
+            from: "<style>",
             to: '<style type="text/css" rel="stylesheet" media="all">',
           },
         ],
@@ -312,8 +315,8 @@ module.exports = function(grunt) {
         files: [
           {
             expand: true,
-            cwd: 'src/templates/',
-            src: ['**/*'],
+            cwd: "src/templates/",
+            src: ["**/*"],
             dest: `${path.dist}postmark-templates`,
           },
         ],
@@ -322,8 +325,8 @@ module.exports = function(grunt) {
         files: [
           {
             expand: true,
-            cwd: 'src/layouts/',
-            src: ['**/*'],
+            cwd: "src/layouts/",
+            src: ["**/*"],
             dest: `${path.dist}postmark-layouts`,
           },
         ],
@@ -348,23 +351,23 @@ module.exports = function(grunt) {
 
     postmark: {
       options: {
-        serverToken: '<%= secret.postmark.server_token %>',
-        from: '<%= config.postmark.from %>',
-        to: '<%= config.postmark.to %>',
-        subject: '<%= config.postmark.subject %>',
+        serverToken: "<%= secret.postmark.server_token %>",
+        from: "<%= config.postmark.from %>",
+        to: "<%= config.postmark.to %>",
+        subject: "<%= config.postmark.subject %>",
       },
       // run "grunt postmark:welcome" - Sends just the welcome email
       welcome: {
-        src: 'dist/compiled/welcome/content.html',
+        src: "dist/compiled/welcome/content.html",
       },
       // run "npm run flood" - Sends all of the emails. Be careful not to spam PM if you have a bunch of emails.
       flood: {
-        src: ['dist/compiled/**/content.html'],
+        src: ["dist/compiled/**/content.html"],
       },
       // run "npm run litmus" - Add a litmus test address here.
       litmus: {
-        to: '<%= config.strings.litmus_email %>',
-        src: 'dist/compiled/user-invitation/content.html',
+        to: "<%= config.strings.litmus_email %>",
+        src: "dist/compiled/user-invitation/content.html",
       },
     },
 
@@ -379,13 +382,12 @@ module.exports = function(grunt) {
         stderr: true,
         execOptions: {
           env: {
-            'POSTMARK_SERVER_TOKEN': '<%= secret.postmark.server_token %>'
-          }
-        }
+            POSTMARK_SERVER_TOKEN: "<%= secret.postmark.server_token %>",
+          },
+        },
       },
       postmarkPush: {
-        command:
-          'postmark templates push ./dist',
+        command: "postmark templates push ./dist",
       },
     },
 
@@ -399,62 +401,62 @@ module.exports = function(grunt) {
         src: [path.dist],
       },
       hbs: {
-        src: ['dist/**/*.hbs'],
+        src: ["dist/**/*.hbs"],
       },
     },
-  })
+  });
 
   /* Tasks
   ================================================= */
 
-  grunt.registerTask('default', [
-    'clean:dist',
-    'css',
-    'postmark-templates',
-    'generic-templates',
-  ])
+  grunt.registerTask("default", [
+    "clean:dist",
+    "css",
+    "postmark-templates",
+    "generic-templates",
+  ]);
 
   /**
    * Compiles Postmark compatible layouts and templates into separate files.
    * Ideal if Postmark is your email service provider.
    * CSS is not inlined since Postmark handles this for you.
    */
-  grunt.registerTask('postmark-templates', [
-    'copy:postmarkTemplates', // Copy Handlebars templates to dist folder
-    'copy:postmarkLayouts', // Copy Handlebars layouts to dist folder
-    'replace:postmarkPlaceholder', // Replace handlebars {{body}} with Postmark content placeholder
-    'replace:postmarkConfig', // Remove handlebars config from layouts
-    'replace:postmarkPreheader', // Insert mustachio syntax on Postmark layout preheaders
-    'assemble:postmark', // Compile handlebars templates and layouts. Mainly used for loading partials into the templates.
-    'inline:postmark', // Load external CSS into layouts
-    'replace:styleBlock', // Add properties to style block
-    'prettify:postmark', // Format layout HTML
-    'premailer:txt', // Generate text versions
-    'replace:variableSyntax', // Clean up variable syntax in text versions
-    'clean:hbs', // Remove handlebars files
-  ])
+  grunt.registerTask("postmark-templates", [
+    "copy:postmarkTemplates", // Copy Handlebars templates to dist folder
+    "copy:postmarkLayouts", // Copy Handlebars layouts to dist folder
+    "replace:postmarkPlaceholder", // Replace handlebars {{body}} with Postmark content placeholder
+    "replace:postmarkConfig", // Remove handlebars config from layouts
+    "replace:postmarkPreheader", // Insert mustachio syntax on Postmark layout preheaders
+    "assemble:postmark", // Compile handlebars templates and layouts. Mainly used for loading partials into the templates.
+    "inline:postmark", // Load external CSS into layouts
+    "replace:styleBlock", // Add properties to style block
+    "prettify:postmark", // Format layout HTML
+    "premailer:txt", // Generate text versions
+    "replace:variableSyntax", // Clean up variable syntax in text versions
+    "clean:hbs", // Remove handlebars files
+  ]);
 
   /**
    * Compiles templates and layouts to dist/compiled.
    * Each template file includes the combined layout with inlined CSS.
    * Ideal if you don't use Postmark as an email service provider.
    */
-  grunt.registerTask('generic-templates', [
-    'assemble:generic', // Compile handlebars templates to HTML
-    'inline:generic', // Load external CSS into templates
-    'premailer', // Inline CSS and generate text version
-    'replace:variableSyntax', // Clean up variable syntax in text versions
-    'replace:styleBlock', // Add properties to style block
-    'prettify:generic', // Format HTML
-  ])
+  grunt.registerTask("generic-templates", [
+    "assemble:generic", // Compile handlebars templates to HTML
+    "inline:generic", // Load external CSS into templates
+    "premailer", // Inline CSS and generate text version
+    "replace:variableSyntax", // Clean up variable syntax in text versions
+    "replace:styleBlock", // Add properties to style block
+    "prettify:generic", // Format HTML
+  ]);
 
-  grunt.registerTask('css', ['sass', 'autoprefixer'])
-  grunt.registerTask('images-ftp', ['ftp-deploy'])
-  grunt.registerTask('images-s3', ['s3'])
-  grunt.registerTask('deploy', ['default', 'shell:postmarkPush'])
+  grunt.registerTask("css", ["sass", "autoprefixer"]);
+  grunt.registerTask("images-ftp", ["ftp-deploy"]);
+  grunt.registerTask("images-s3", ["s3"]);
+  grunt.registerTask("deploy", ["default", "shell:postmarkPush"]);
 
   // Testing
-  grunt.registerTask('spam', ['spamcheck'])
-  grunt.registerTask('litmus', ['default', 'postmark:litmus'])
-  grunt.registerTask('flood', ['default', 'postmark:flood'])
-}
+  grunt.registerTask("spam", ["spamcheck"]);
+  grunt.registerTask("litmus", ["default", "postmark:litmus"]);
+  grunt.registerTask("flood", ["default", "postmark:flood"]);
+};
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 <p align="center">Brought to you by <a href="http://postmarkapp.com"><img src="http://assets.wildbit.com/postmark/misc/postmark.svg" alt="Postmark"></a>
 </p>
 
-*Few tasks are more tedious than building a consistent set of well-tested and beautiful transactional email templates for your application.* Not any longer.
+_Few tasks are more tedious than building a consistent set of well-tested and beautiful transactional email templates for your application._ Not any longer.
 
 With MailMason, you can use Grunt tasks, Handlebars, and SASS to streamline building a consistent set of transactional email templates using layouts and partials to reduce redundancy and create both the HTML and plain text versions of your transactional emails in one fell swoop.
 
@@ -14,16 +14,16 @@ By default, the generated templates use [Mustachio](https://github.com/wildbit/m
 
 ## What does it do for you?
 
-* [X] Gives you a thoroughly tested and reliable starting point for building consistent email templates
-* [X] Uses Handlebars to enable layouts and partials in templates and avoid redundancy
-* [X] Enables the use of SCSS for generating the styles
-* [X] Automatically inlines the resulting CSS for testing email client compatibility
-* [X] Automatically generates text versions of emails with the same content as the HTML versions
-* [X] Enables easy sending of test emails through your Postmark account
-* [X] Enables easy batch testing against the [Postmark Spamcheck API](http://spamcheck.postmarkapp.com)
-* [X] Enables easy batch testing through [Litmus](http://litmus.com)
-* [X] Enables easy uploading of image assets to your CDN so you can include images in your templates (but you don't have to)
-* [X] Enables easy template pushing to your Postmark account
+- [x] Gives you a thoroughly tested and reliable starting point for building consistent email templates
+- [x] Uses Handlebars to enable layouts and partials in templates and avoid redundancy
+- [x] Enables the use of SCSS for generating the styles
+- [x] Automatically inlines the resulting CSS for testing email client compatibility
+- [x] Automatically generates text versions of emails with the same content as the HTML versions
+- [x] Enables easy sending of test emails through your Postmark account
+- [x] Enables easy batch testing against the [Postmark Spamcheck API](http://spamcheck.postmarkapp.com)
+- [x] Enables easy batch testing through [Litmus](http://litmus.com)
+- [x] Enables easy uploading of image assets to your CDN so you can include images in your templates (but you don't have to)
+- [x] Enables easy template pushing to your Postmark account
 
 ## Interested in contributing?
 
@@ -33,28 +33,28 @@ Read through our [guidelines for contributing](https://github.com/wildbit/mailma
 
 If you need help getting started or using MailMason, make sure to check out the [MailMason wiki](https://github.com/wildbit/mailmason/wiki).
 
-* [About](https://github.com/wildbit/mailmason/wiki/About)
-  * [What can it do?](https://github.com/wildbit/mailmason/wiki/About#what-can-it-do)
-  * [What templates are included?](https://github.com/wildbit/mailmason/wiki/About#what-templates-are-included)
-* [Getting Started](https://github.com/wildbit/mailmason/wiki/Getting-Started)
-  * [Setup](https://github.com/wildbit/mailmason/wiki/Getting-Started#setup)
-  * [Configuration](https://github.com/wildbit/mailmason/wiki/Getting-Started#configuration)
-    * [Secrets.json](https://github.com/wildbit/mailmason/wiki/Getting-Started#secretsjson)
-    * [Config.json](https://github.com/wildbit/mailmason/wiki/Getting-Started#secretsjson)
-    * [Images & Assets](https://github.com/wildbit/mailmason/wiki/Getting-Started#images--assets)
-    * [Social Images](https://github.com/wildbit/mailmason/wiki/Getting-Started#social-images)
-* [Project Structure](https://github.com/wildbit/mailmason/wiki/Project-Structure)
-  * [Templates](https://github.com/wildbit/mailmason/wiki/Project-Structure#templates)
-  * [Images](https://github.com/wildbit/mailmason/wiki/Project-Structure#images)
-  * [Layouts](https://github.com/wildbit/mailmason/wiki/Project-Structure#layouts)
-  * [Partials](https://github.com/wildbit/mailmason/wiki/Project-Structure#partials)
-  * [Stylesheets](https://github.com/wildbit/mailmason/wiki/Project-Structure#stylesheets)
-* [Usage](https://github.com/wildbit/mailmason/wiki/Usage)
-  * [Building](https://github.com/wildbit/mailmason/wiki/Usage#building)
-  * [Testing](https://github.com/wildbit/mailmason/wiki/Usage#testing)
-  * [Pushing templates to Postmark](https://github.com/wildbit/mailmason/wiki/Usage#pushing-templates-to-postmark)
-* [Development](https://github.com/wildbit/mailmason/wiki/Development)
-* [Templates](https://github.com/wildbit/mailmason/wiki/Templates)
+- [About](https://github.com/wildbit/mailmason/wiki/About)
+  - [What can it do?](https://github.com/wildbit/mailmason/wiki/About#what-can-it-do)
+  - [What templates are included?](https://github.com/wildbit/mailmason/wiki/About#what-templates-are-included)
+- [Getting Started](https://github.com/wildbit/mailmason/wiki/Getting-Started)
+  - [Setup](https://github.com/wildbit/mailmason/wiki/Getting-Started#setup)
+  - [Configuration](https://github.com/wildbit/mailmason/wiki/Getting-Started#configuration)
+    - [Secrets.json](https://github.com/wildbit/mailmason/wiki/Getting-Started#secretsjson)
+    - [Config.json](https://github.com/wildbit/mailmason/wiki/Getting-Started#secretsjson)
+    - [Images & Assets](https://github.com/wildbit/mailmason/wiki/Getting-Started#images--assets)
+    - [Social Images](https://github.com/wildbit/mailmason/wiki/Getting-Started#social-images)
+- [Project Structure](https://github.com/wildbit/mailmason/wiki/Project-Structure)
+  - [Templates](https://github.com/wildbit/mailmason/wiki/Project-Structure#templates)
+  - [Images](https://github.com/wildbit/mailmason/wiki/Project-Structure#images)
+  - [Layouts](https://github.com/wildbit/mailmason/wiki/Project-Structure#layouts)
+  - [Partials](https://github.com/wildbit/mailmason/wiki/Project-Structure#partials)
+  - [Stylesheets](https://github.com/wildbit/mailmason/wiki/Project-Structure#stylesheets)
+- [Usage](https://github.com/wildbit/mailmason/wiki/Usage)
+  - [Building](https://github.com/wildbit/mailmason/wiki/Usage#building)
+  - [Testing](https://github.com/wildbit/mailmason/wiki/Usage#testing)
+  - [Pushing templates to Postmark](https://github.com/wildbit/mailmason/wiki/Usage#pushing-templates-to-postmark)
+- [Development](https://github.com/wildbit/mailmason/wiki/Development)
+- [Templates](https://github.com/wildbit/mailmason/wiki/Templates)
 
 ## License
 
